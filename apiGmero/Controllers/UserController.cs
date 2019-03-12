7e95493403a8e6499f9aefbc7883ca4c168d4b64
geovanny mero <jefferson_mero1911@hotmail.com>
2019-03-12 13:32:08 -0500

    my first webapi

---
+++ apiGmero/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using apiGmero.Models;
+
+namespace apiGmero.Controllers
+{
+    public class UserController : ApiController
+    {
+
+        private Users[] users = new Users[]
+        {
+            new Users
+            {
+                Id = 1,
+                Identificacion = "0931254569",
+                Nombre = "Geovanny",
+                Apellidos = "Mero Baque",
+                FechaNacimiento = Convert.ToDateTime("19/11/1993")
+            }
+        };
+        // GET api/user
+        public IEnumerable<Users> Get()
+        {
+            return users;
+        }
+
+        // GET api/user/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/user
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/user/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/user/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ apiGmero/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("apiGmero")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("apiGmero")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en True en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como ID de typelib si este proyecto se expone a COM
+[assembly: Guid("adeb1b9d-6750-4ab3-a102-decc60f9f0e6")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
